f3f4fbf8222d6b0fcf3778eb1f1463e7a6facf1d
KimHyunjun <khj@zeonplex.co.kr>
2019-07-15 19:38:42 +0900

    Update example.js

--- commands/example.js
+++ commands/example.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js')
 
-module.exports.run = async (bot, message, args) => {
+module.exports.run = async (client, message, args) => {
     // You can insert your command code here.
 }
 
