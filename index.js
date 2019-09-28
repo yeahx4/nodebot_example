cc9b226a88c0523a02adf6e627bf4360eae2ef22
KimHyunjun <khj@zeonplex.co.kr>
2019-09-28 12:40:39 +0900

    korean-logger using

--- index.js
+++ index.js
@@ -1,28 +1,29 @@
 const Discord = require('discord.js')
 const client = new Discord.Client()
 const fs = require('fs')
+const klog = require('korean-logger')
 client.commands = new Discord.Collection()
 
 fs.readdir('./commands/', (err, files) => {
 
-  if(err) console.log(err)
+  if(err) klog.error(err)
 
   let jsfile = files.filter(f => f.split('.').pop() === 'js')
   if(jsfile.length <= 0) {
-    console.log('Failed to load command!')
+    klog.warn('Failed to load command!')
     return
   }
 
   jsfile.forEach((f, i) => {
     let props = require(`./commands/${f}`)
-    console.log(`${f} Loaded!`)
+    klog.success(`${f} Loaded!`)
     client.commands.set(props.help.name, props)
   })
 
 })
 
 client.on('ready', () => {
-  console.log(`Online ${client.user.tag}`);
+  klog.alert(`Online ${client.user.tag}`);
 });
 
 client.on('message', message => {
